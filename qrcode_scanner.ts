26a488542a2f328ddf423f1a39dfed169b59e744
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2022-12-30 22:05:05 +0100

    Scanner auto opens links

--- qrcode_scanner.ts
+++ qrcode_scanner.ts
@@ -1,13 +1,16 @@
 import QrScanner from "./node_modules/qr-scanner/qr-scanner.min.js";
 export async function scan() {
-  console.log("Test");
   if (!await QrScanner.hasCamera()) return;
-  console.log(QrScanner.listCameras);
 
   const qrScanner = new QrScanner(
     document.getElementById("scanner"),
-    (result: string) => console.log("decoded qr code:", result),
+    (result: { data: string }) => onResult(qrScanner, result.data),
     {/* your options or returnDetailedScanResult: true if you're not specifying any other options */},
   );
   qrScanner.start();
+}
+
+function onResult(qrScanner: QrScanner, result: string) {
+  window.open(result);
+  qrScanner.stop();
 }
